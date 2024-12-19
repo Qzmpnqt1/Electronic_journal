774041b880eaa10b7405582c9063a6383a5dea47
Qzmpnqt1 <sgoppen@gmail.com>
2024-12-19 23:09:29 +0300

    fix

--- app/src/main/java/com/example/electronic_journal/MainActivity.kt
+++ app/src/main/java/com/example/electronic_journal/MainActivity.kt
@@ -2,39 +2,11 @@ package com.example.electronic_journal
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        drawerLayout = findViewById(R.id.drawer_layout)
-        val navController = findNavController(R.id.nav_host_fragment)
-        val navView: NavigationView = findViewById(R.id.nav_view)
-
-        appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.authorizationFragment, R.id.studentFragment, R.id.teacherFragment, R.id.administratorFragment
-            ),
-            drawerLayout
-        )
-
-        // Устанавливаем NavigationView с NavController
-        navView.setupWithNavController(navController)
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment)
-        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
